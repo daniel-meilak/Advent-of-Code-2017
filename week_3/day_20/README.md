4f247d79e31fa9d22bd16a1a30e9d4cf48182f82
Daniel <daniel.mlak@gmail.com>
2021-01-16 23:48:32 +0100

    Finished day 20

---
+++ week_3/day_20/README.md
@@ -0,0 +1,5 @@
+## Day 20
+
+Part 1 can probably be optimized in some way which does not involve actually simulating the particles. 
+
+Part 2 is done very inefficiently as we recheck collided particles over and over instead of removing them from consideration. 
---
+++ week_3/day_20/day_20.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cstdlib>
+#include"../../Utils/utils.h"
+
+int main(){
+
+    // read input into vector of vector of strings.
+    std::vector<std::string> delimiters = {"p", "=", "<", ">", ",", "v", "a", " "};
+    std::vector<std::vector<long long>> input = input_to_llint_2D(read_input_2D("input", delimiters));
+
+    // no. of particles
+    const int particles = input.size();
+    
+    // manhattan distance of each particle
+    std::vector<long long> manhattan(particles);
+
+    // num of ticks
+    const int ticks = 10000;
+
+    // collided particles
+    std::vector<bool> collided(particles, false);
+
+    // simulate particles
+    for (int i=0; i<ticks; i++){
+
+        // move each particle
+        for (int j=0; j<particles; j++){
+
+            // inc velocity using acceleration
+            input[j][3] += input[j][6];
+            input[j][4] += input[j][7];
+            input[j][5] += input[j][8];
+
+            // inc position using velocity
+            input[j][0] += input[j][3];
+            input[j][1] += input[j][4];
+            input[j][2] += input[j][5];
+
+            // calculate manhattan distances
+            manhattan[j] = manhattan_3D(input[j][0], input[j][1], input[j][2]);
+        }
+
+        // check for collisions
+        for (int x=0; x<particles-1; x++){
+            for (int y=x+1; y<particles; y++){
+                if (input[x][0]==input[y][0] && input[x][1]==input[y][1] && input[x][2]==input[y][2]){
+                    collided[x] = true;
+                    collided[y] = true;
+                }
+            }
+        }
+    }
+
+    // find particle closest to origin after simulation
+    int closest = std::distance(manhattan.begin(), std::min_element(manhattan.begin(), manhattan.end()));
+
+    // check how many particles did not collide
+    int count = 0;
+    for ( bool p : collided){
+        if (!p){ count++; }
+    }
+
+    std::cout << "Answer (part 1): " << closest << std::endl;
+    std::cout << "Answer (part 2): " << count   << std::endl;
+
+    return 0;
+}
